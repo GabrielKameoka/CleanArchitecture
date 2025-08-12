b4d1e7e467bf664e08d6d430b94bc0c25db992de
GabrielKameoka <gabrielkameoka@gmail.com>
2025-08-12 15:43:18 -0300

    Single Responsability Priciple

---
+++ SOLID/ProjetoSRP/ProjetoSRP/Entities/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoSRP.Entities;
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string CPF { get; set; }
+
+    public Pessoa(string nome, string email, string cpf)
+    {
+        Nome = nome;
+        Email = email;
+        CPF = cpf;
+    }
+}
---
+++ SOLID/ProjetoSRP/ProjetoSRP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ProjetoSRP.Banco;
+using ProjetoSRP.Entities;
+using ProjetoSRP.Services;
+
+namespace ProjetoSRP;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Pessoa pessoa1 = new Pessoa("Gabriella", "gabriella@gmail.com", "12345678910");
+        BancoPedidos banco = new BancoPedidos();
+        PedidoRepository pedidoRepo = new PedidoRepository(banco);
+        Pedido pedido = new Pedido();
+        
+        Console.WriteLine($"Nome: {pessoa1.Nome};");
+        Console.WriteLine($"Email: {pessoa1.Email};");
+        Console.WriteLine($"CPF: {pessoa1.CPF};");
+
+        Produto p1 = new Produto("TV", 1000);
+        Produto p2 = new Produto("Celular", 900);
+
+        pedido.AdicionarProduto(p1);
+        pedido.AdicionarProduto(p2);
+
+        foreach (var produto in pedido.Produtos)
+        {
+            Console.WriteLine($"Produto: {produto.NomeProduto}, Preço: {produto.Preco} ");
+        }
+        
+        Console.WriteLine($"Total do pedido: R$ {pedido.CalculaTotal()}");
+
+        pedidoRepo.AdicionarPedido(pedido);
+
+        Console.WriteLine($"Pedidos no banco: {pedidoRepo.ObterPedidos()}");
+    }
+}
